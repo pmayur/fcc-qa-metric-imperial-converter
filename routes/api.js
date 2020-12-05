1b1829bcbce7175592beba45a96a0518a533a4d6
pmayur <thisispatil@gmail.com>
2020-12-05 23:28:00 +0530

    add: api for converting imperial values

--- routes/api.js
+++ routes/api.js
@@ -17,14 +17,35 @@ module.exports = function (app) {
 
   app.route('/api/convert')
     .get(function (req, res){
+      
+      // input from the request
       let input = req.query.input;
+
+      // parse input to separate number and unit
       let initNum = convertHandler.getNum(input);
       let initUnit = convertHandler.getUnit(input);
+      
+      // both number and unit provided are invalid
+      if(!initNum && !initUnit) return res.status(400).send("invalid number and unit")
+
+      // number provided invalid
+      if(!initNum) return res.status(400).send("invalid number")
+
+      // unit provided invalid
+      if(!initUnit) return res.status(400).send("invalid unit")
+
       let returnNum = convertHandler.convert(initNum, initUnit);
       let returnUnit = convertHandler.getReturnUnit(initUnit);
+
       let toString = convertHandler.getString(initNum, initUnit, returnNum, returnUnit);
-      
-      //res.json
+
+      res.json({
+        "initNum": parseFloat(initNum),
+        "initUnit": initUnit,
+        "returnNum": parseFloat(returnNum),
+        "returnUnit": returnUnit,
+        "string": toString
+      })
     });
     
 };
